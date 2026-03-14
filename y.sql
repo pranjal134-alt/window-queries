692e542e6557c37cb5792edfa7032f9cc8252e3b
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-03-14 13:16:26 +0530

    Add files via upload

---
+++ y.sql
@@ -0,0 +1,15 @@
+create database school;
+drop database school;
+use school;
+create table  sto
+( 
+id int not null,
+name varchar (45));
+insert into sto values(1,"pranjal"),(2,"vidhan");
+select * from sto
+where id>1;
+create table kto(
+name varchar(23),
+age int not null
+)
+insert into sto values( 56,"pranjal"),(32,"tinu");
